d9755c423ba8b24fa855e4926014ab5353fcc3c6
AdrianaWinchester <adriana_rieger@gmx.de>
2022-06-21 15:36:13 +0200

    Movement: added dynamic scale change

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -13,14 +14,30 @@ public class PlayerMovement : MonoBehaviour
     Vector2 lastClickedPosistion;
     
     bool moving;
+    public float SCALERATIO; //Groß geschrieben, wel magic number
+    float scaleDifference;
 
+    Vector3 scaleChange;
     // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButtonDown(0)) //0 ist die linke Maustaste
         {
             lastClickedPosistion = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            scaleDifference = Math.Abs((lastClickedPosistion.y - transform.position.y) * SCALERATIO); //gibt immer einen positiven Wert zurück
+            Debug.Log("Scale Difference = " + scaleDifference);
+            Debug.Log(new Vector3(scaleDifference, scaleDifference, scaleDifference));
             moving = true;
+            if (lastClickedPosistion.y > transform.position.y)
+            {
+                this.gameObject.transform.localScale -= new Vector3(scaleDifference, scaleDifference, scaleDifference);
+                Debug.Log("Geht nach HINTEN");
+            }
+            else if (lastClickedPosistion.y < transform.position.y)
+            {
+                this.gameObject.transform.localScale += new Vector3(scaleDifference, scaleDifference, scaleDifference);
+                Debug.Log("Geht nach VORNE");
+            }
         }
 
         if (moving && (Vector2)transform.position != lastClickedPosistion)
@@ -41,6 +58,8 @@ public class PlayerMovement : MonoBehaviour
                 this.gameObject.GetComponent<SpriteRenderer>().sprite = PlayerStanding;
                 Debug.Log("STEHT");
             }
+
+            
         }
         else
         {

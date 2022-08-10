c9f82948f72a169d09bdb2c440ba081981dd5a6a
AdrianaWinchester <adriana_rieger@gmx.de>
2022-08-10 16:51:14 +0200

    added Prof and adjusted scale added bg daydream

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -19,26 +19,29 @@ public class PlayerMovement : MonoBehaviour
     RaycastHit2D hit;
     Vector3 scaleChange;
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         
         if (Input.GetMouseButtonDown(0)) //0 ist die linke Maustaste
         {
             lastClickedPosistion = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             scaleDifference = Math.Abs((lastClickedPosistion.y - transform.position.y) * SCALERATIO); //gibt immer einen positiven Wert zur�ck
-            Debug.Log("Scale Difference = " + scaleDifference);
-            Debug.Log(new Vector3(scaleDifference, scaleDifference, scaleDifference));
+            //Debug.Log("Scale Difference = " + scaleDifference);
+            //Debug.Log(new Vector3(scaleDifference, scaleDifference, scaleDifference));
             hit = Physics2D.Raycast(lastClickedPosistion, Vector2.zero);
             moving = true;
             if (lastClickedPosistion.y > transform.position.y && hit.collider.gameObject.tag == "Ground")
             {
                 this.gameObject.transform.localScale -= new Vector3(scaleDifference, scaleDifference, scaleDifference);
-                Debug.Log("Geht nach HINTEN");
+                //Debug.Log("Geht nach HINTEN");
             }
             else if (lastClickedPosistion.y < transform.position.y && hit.collider.gameObject.tag == "Ground")
             {
                 this.gameObject.transform.localScale += new Vector3(scaleDifference, scaleDifference, scaleDifference);
-                Debug.Log("Geht nach VORNE");
+                //Debug.Log("Geht nach VORNE");
+            }else
+            {
+                Debug.Log("Dort kann man nicht hinlaufen");
             }
         }
 
@@ -46,22 +49,22 @@ public class PlayerMovement : MonoBehaviour
 
         if (moving && (Vector2)transform.position != lastClickedPosistion && hit.collider.gameObject.tag == "Ground")
         {
-            Debug.Log(hit.collider.gameObject.tag);
+            //Debug.Log(hit.collider.gameObject.tag);
             float go = speed * Time.deltaTime;
             transform.position = Vector2.MoveTowards(transform.position, lastClickedPosistion, go);
             if (lastClickedPosistion.x > transform.position.x)
             {
                 this.gameObject.GetComponent<SpriteRenderer>().sprite = PlayerRight;
-                Debug.Log("Geht nach rechts");
+                //Debug.Log("Geht nach rechts");
             }
             else if (lastClickedPosistion.x < transform.position.x)
             {
                 this.gameObject.GetComponent<SpriteRenderer>().sprite = PlayerLeft;
-                Debug.Log("Geht nach links");
+               //Debug.Log("Geht nach links");
             } else
             {
                 this.gameObject.GetComponent<SpriteRenderer>().sprite = PlayerStanding;
-                Debug.Log("STEHT");
+                //Debug.Log("STEHT");
             }
 
             
